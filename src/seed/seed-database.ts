400ffc4f3f60d1c15a4837123ff1c097b129a311
vcontreras <contrerastrece@gmail.com>
2024-09-05 17:01:42 -0500

    seed-country

--- src/seed/seed-database.ts
+++ src/seed/seed-database.ts
@@ -1,10 +1,12 @@
 import { initialData } from "./seed";
 import prisma from "../lib/prisma";
+import { countries } from "./seed-countries";
 
 async function main() {
   // 1. Borrar registros previos
   // await Promise.all( [
   await prisma.user.deleteMany();
+  await prisma.country.deleteMany();
 
   await prisma.productImage.deleteMany();
   await prisma.product.deleteMany();
@@ -14,6 +16,7 @@ async function main() {
   const { categories, products, users } = initialData;
 
   await prisma.user.createMany({ data: users });
+  await prisma.country.createMany({ data: countries });
 
   const categoriesData = categories.map((name) => ({ name }));
 
