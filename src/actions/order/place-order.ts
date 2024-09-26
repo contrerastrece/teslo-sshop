cb3e375888a67d0ba81c06c7286e698c2eed9ef4
vcontreras <contrerastrece@gmail.com>
2024-09-26 13:52:57 -0500

    actualizar inventario tx prisma

--- src/actions/order/place-order.ts
+++ src/actions/order/place-order.ts
@@ -12,6 +12,7 @@ interface ProductToOrder {
 
 export const placeOrder = async (productsId: ProductToOrder[], address: Address) => {
   console.log(address)
+  console.log(productsId)
 
   try {
     const session = await auth();
@@ -55,59 +56,106 @@ export const placeOrder = async (productsId: ProductToOrder[], address: Address)
     );
 
 
-    // crear la transacción de base de datos
-    const prismaTx = await prisma.$transaction(async (tx) => {
+    try {
 
-      // 1. Actualizar el stock de los productos
+      // crear la transacción de base de datos
+      const prismaTx = await prisma.$transaction(async (tx) => {
+
+        // 1. Actualizar el stock de los productos
+
+        const updateProductsPromises = products.map((product) => {
+          // acumular los valores
+          const productQuantity = productsId.filter(
+            p => p.productId === product.id
+          ).reduce((acc, item) => item.quantity + acc, 0);
+
+
+          if (productQuantity === 0) {
+            throw new Error(`${product.id} no tiene cantidad definida`)
+          }
+
+
+
+          return tx.product.update({
+            where: { id: product.id },
+            data: {
+              // inStock: product.inStock - productQuantity //no hacer
+              inStock: { decrement: productQuantity },
+            }
+          })
+        })
+
+        const updateProducts = await Promise.all(updateProductsPromises)
+
+        updateProducts.forEach(product => {
+          if (product.inStock < 0) {
+            throw new Error(`${product.title} no hay inventario suficiente`)
+          }
+        })
 
 
 
 
-      // 2. Crear la orden - Encabezado - Detalles
-      const order = await tx.order.create({
-        data: {
-          userId: userId,
-          itemsInOrder: itemsInOrder,
-          subTotal: subTotal,
-          tax: tax,
-          total: total,
+        // 2. Crear la orden - Encabezado - Detalles
+        const order = await tx.order.create({
+          data: {
+            userId: userId,
+            itemsInOrder: itemsInOrder,
+            subTotal: subTotal,
+            tax: tax,
+            total: total,
 
-          OrderItem: {
-            createMany: {
-              data: productsId.map(p => ({
-                quantity: p.quantity,
-                size: p.size,
-                productId: p.productId,
-                price: products.find(product => product.id === p.productId)?.price ?? 0
-              }))
+            OrderItem: {
+              createMany: {
+                data: productsId.map(p => ({
+                  quantity: p.quantity,
+                  size: p.size,
+                  productId: p.productId,
+                  price: products.find(product => product.id === p.productId)?.price ?? 0
+                }))
+              }
+
             }
 
           }
+        })
 
+        // validar, si el pricio es cero, entonces lanzar un error
+
+
+        // 3. Crear la dirección de la orden
+        const { country, ...restAddress } = address;
+        const orderAddress = await tx.orderAddress.create({
+          data: {
+            ...restAddress,
+            countryId: country,
+            orderId: order.id
+          }
+        })
+
+
+
+        return {
+          order: order,
+          updateProduct: updateProducts,
+          orderAddress: orderAddress
         }
       })
-
-      // validar, si el pricio es cero, entonces lanzar un error
-
-
-      // 3. Crear la dirección de la orden
-      const { country, ...restAddress } = address;
-      const orderAddress = await tx.orderAddress.create({
-        data: {
-          ...restAddress,
-          countryId: country,
-          orderId: order.id
-        }
-      })
-
 
 
       return {
-        order: order,
-        updateProduct: [],
-        orderAddress: orderAddress
+        ok: true,
+        order: prismaTx.order.id,
+        prismaTx: prismaTx
       }
-    })
+
+    } catch (error: any) {
+      return {
+        ok: false,
+        message: error?.message
+      }
+    }
+
 
 
 
